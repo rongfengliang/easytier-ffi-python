723e493352b8b4aceaa7226e114e797e674e7f7a
rongfengliang <1141591465@qq.com>
2025-06-10 22:00:27 +0800

    add more demo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import easytier
 import time
+from easytier._easytier_cffi import ffi
 easy = easytier.EasyTier()
 
 with open("app.yaml","r") as f:
@@ -12,10 +13,19 @@ if result != 0:
 else:
     print("Configuration parsed successfully.")
 
+max_length = 10
 result = easy.run_network_instance(config)
+
+infos = ffi.new("KeyValuePair[]", max_length)
+
 
 if result == 0:
     print("Network instance started successfully.")
     while True:
-        time.sleep(1)
+        time.sleep(3)
+        ret = easy.libeasytier.collect_network_infos(infos, max_length)
+        for i in range(ret):
+            key = ffi.string(infos[i].key).decode()
+            value = ffi.string(infos[i].value).decode()
+            print(f"{key}: {value}")
         print("Running network instance...")
